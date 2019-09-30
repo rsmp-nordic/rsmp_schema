15394dfd2a69ff18b2ba7f54d6117a2139c77495
Emil Tin <zf0f@tmf.kk.dk>
2019-09-30 14:14:17 +0200

    validate Version message

--- spec/tlc/version_spec.rb
+++ spec/tlc/version_spec.rb
@@ -19,12 +19,75 @@ RSpec.describe "Traffic Light Controller RSMP SXL Schema validation" do
 	  expect( validate(message) ).to be_nil
   end
 
-  it 'catches missing site id' do
+  it 'catches missing RSMP version' do
 		invalid = message.dup
-		invalid.delete 'siteId'
+		invalid.delete 'RSMP'
 	  expect( validate(invalid) ).to eq([
-	  	["", "required", {"missing_keys"=>["siteId"]}]
+	  	["", "required", {"missing_keys"=>["RSMP"]}]
 	  ])
   end
 
+  it 'catches bad RSMP format, must be array' do
+		invalid = message.dup
+		invalid['RSMP'] = '1.0'
+	  expect( validate(invalid) ).to eq([
+	  	["/RSMP", "array"]
+	  ])
+  end
+
+  it 'catches bad RSMP format, array cannot be empty' do
+		invalid = message.dup
+		invalid['RSMP'] = []
+	  expect( validate(invalid) ).to eq([
+	  	["/RSMP", "minItems"]
+	  ])
+  end
+
+  it 'catches bad RSMP format, item must be hash' do
+		invalid = message.dup
+		invalid['RSMP'] = ['1.0']
+	  expect( validate(invalid) ).to eq([
+	  	["/RSMP/0", "object"]
+	  ])
+  end
+
+  it 'catches bad RSMP format, item must have version' do
+		invalid = message.dup
+		invalid['RSMP'] = [{}]
+	  expect( validate(invalid) ).to eq([
+	  	["/RSMP/0", "required", {"missing_keys"=>["vers"]}]
+	  ])
+  end
+
+  it 'catches bad RSMP format, item cannot have extra attributes' do
+		invalid = message.dup
+		invalid['RSMP'] = [{'vers'=>'1.0.0','extra'=>'123'}]
+	  expect( validate(invalid) ).to eq([
+	  	["/RSMP/0/extra", "schema"]
+	  ])
+  end
+
+  it 'catches bad RSMP format, version must be 1.0.0 format' do
+		invalid = message.dup
+		invalid['RSMP'].first['vers'] = 'latest'
+	  expect( validate(invalid) ).to eq([
+	  	["/RSMP/0/vers", "pattern"]
+	  ])
+  end
+
+  it 'catches missing SXL version' do
+		invalid = message.dup
+		invalid.delete 'SXL'
+	  expect( validate(invalid) ).to eq([
+	  	["", "required", {"missing_keys"=>["SXL"]}]
+	  ])
+  end
+
+  it 'catches bad SXL version' do
+		invalid = message.dup
+		invalid['SXL'] = '1.0.1-preview'
+	  expect( validate(invalid) ).to eq([
+	  	["/SXL", "pattern"]
+	  ])
+  end
 end
