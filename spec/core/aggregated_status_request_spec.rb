837d76fa1cd398bfcf2b5541af51de0ba29d6146
Emil Tin <zf0f@tmf.kk.dk>
2022-08-12 10:37:01 +0200

    add spec for aggregated status request

---
+++ spec/core/aggregated_status_request_spec.rb
@@ -0,0 +1,25 @@
+RSpec.describe "Traffic Light Controller RSMP SXL Schema validation" do
+	let(:message) {{
+		"mType" => "rSMsg",
+		"type" => "AggregatedStatusRequest",
+		"mId" => "be12ab9a-800c-4c19-8c50-adf832f22420",
+		"cId" => "O+14439=481WA001",
+	}}
+
+	it 'accepts valid aggregated request from 3.1.5' do
+	  expect( validate(message, 'core', '3.1.5') ).to be_nil
+  end
+
+	it 'reject valid alarm aggregated before 3.1.5' do
+	  expect( validate(message, 'core', ['3.1.1','3.1.2','3.1.3','3.1.4']) ).to eq(
+	  	[["/type", "enum"]]
+	  )
+  end
+
+  it 'catches missing mId' do
+		message.delete 'mId'
+	  expect( validate(message, 'core', '3.1.5') ).to eq(
+	  	[["", "required", {"missing_keys"=>["mId"]}]]
+	  )
+  end
+end
