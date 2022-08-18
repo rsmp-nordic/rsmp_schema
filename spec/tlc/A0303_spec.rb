bc560a18a4e1ce34fa3eeac6e2ff6f8d1d5fe30e
Emil Tin <zf0f@tmf.kk.dk>
2022-08-18 09:07:28 +0200

    add A0303 and A0304 to sxl 1.1

---
+++ spec/tlc/A0303_spec.rb
@@ -0,0 +1,49 @@
+RSpec.describe "Traffic Light Controller RSMP SXL Schema validation" do
+
+  let(:message) {{
+    "mType" => "rSMsg",
+    "type" => "Alarm",
+    "mId" => "efb6a4c5-f2ea-4947-9deb-667756926203",
+    "ntsOId" => "KK+AG9998=001TC000",
+    "xNId" => "",
+    "cId" => "KK+AG9998=001DL001",
+    "aCId" => "A0303",
+    "xACId" => "ERROR DETECTOR LOGIC OPEN #1",
+    "xNACId" => "",
+    "aSp" => "Issue",
+    "ack" => "notAcknowledged",
+    "aS" => "Active",
+    "sS" => "notSuspended",
+    "aTs" => "2021-12-13T09:35:25.602Z",
+    "cat" => "D",
+    "pri" => "2",
+    "rvs" => [
+      {
+        "n" => "detector",
+        "v" => "1"
+      },
+      {
+        "n" => "type",
+        "v" => "loop"
+      },
+      {
+        "n" => "errormode",
+        "v" => "on"
+      },
+      {
+        "n" => "manual",
+        "v" => "True"
+      }
+    ]
+  }}
+
+  it 'accepts from 1.1' do
+    expect( validate(message,'tlc', '>=1.1') ).to be_nil
+   end
+
+  it 'reject before 1.1' do
+    expect( validate(message,'tlc', '<1.1') ).to eq(
+      [["/aCId", "enum"]]
+    )
+   end
+end
