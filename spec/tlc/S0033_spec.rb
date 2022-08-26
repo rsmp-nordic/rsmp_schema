6c4a2497b1f11963f821a5ae3e27a23bd3780bde
Emil Tin <zf0f@tmf.kk.dk>
2022-08-26 09:46:58 +0200

    allow native json array in status updates from core 1.1

--- spec/tlc/S0033_spec.rb
+++ spec/tlc/S0033_spec.rb
@@ -83,11 +83,63 @@ RSpec.describe "Traffic Light Controller RSMP SXL Schema validation" do
     ]
   }}
 
+  let(:update) {{
+    "mType" => "rSMsg",
+    "type" => "StatusUpdate",
+    "mId" => "f1a13213-b90a-4abc-8953-2b8142923c55",
+    "ntsOId" => "KK+AG9998=001TC000",
+    "xNId" => "",
+    "cId" => "KK+AG9998=001TC000",
+    "sTs" => "2021-12-13T11:11:07.317Z",
+    "sS" => [
+      {
+        "sCI" => "S0033",
+        "n" => "status",
+        "q" => "recent",
+        "s" => [
+          {
+            "r" => "f90c",
+            "t" => "2021-11-09T15:06:38.796Z",
+            "s" => "received"
+          },
+          {
+            "r" => "uhnv",
+            "t" => "2021-11-09T15:04:12.348Z",
+            "s" => "completed",
+            "e" => "5",
+            "d" => "10"
+          },
+          {
+            "r" => "oh0i",
+            "t" => "2021-11-09T15:06:38.796Z",
+            "s" => "activated"
+          },
+          {
+            "r" => "f90c",
+            "t" => "2021-11-09T15:06:39.796Z",
+            "s" => "completed"
+          },
+          {
+            "r" => "3ia2",
+            "t" => "2021-11-09T15:06:48.796Z",
+            "s" => "queued",
+          },
+          {
+            "r" => "5hc0",
+            "t" => "2021-11-09T15:06:48.796Z",
+            "s" => "timeout"
+          }
+        ]
+      }
+    ]
+  }}
+
 
   it 'accepts from 1.1' do
     expect( validate(request, 'tlc', '>=1.1') ).to be_nil
     expect( validate(response, 'tlc', '>=1.1') ).to be_nil
     expect( validate(empty_response, 'tlc', '>=1.1') ).to be_nil
+    expect( validate(update, 'tlc', '>=1.1') ).to be_nil
   end
 
   it 'rejects before 1.1' do
@@ -102,6 +154,11 @@ RSpec.describe "Traffic Light Controller RSMP SXL Schema validation" do
       ]
     )
     expect( validate(empty_response, 'tlc', '<1.1') ).to eq(
+      [        
+        ["/sS/0/sCI", "enum"]
+      ]
+    )
+    expect( validate(update, 'tlc', '<1.1') ).to eq(
       [        
         ["/sS/0/sCI", "enum"]
       ]
