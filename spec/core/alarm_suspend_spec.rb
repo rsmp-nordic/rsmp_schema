381c6a12b6f84d59e3d8c6daed8a692358364614
Emil Tin <emil@tin.dk>
2024-09-19 10:36:10 +0200

    allow only capitalized Suspend/Resume for alarm aSp from core 3.1.4

--- spec/core/alarm_suspend_spec.rb
+++ spec/core/alarm_suspend_spec.rb
@@ -13,4 +13,12 @@ RSpec.describe "Traffic Light Controller RSMP SXL Schema validation" do
   it 'accepts valid alarm suspend' do
     expect( validate(message, 'core') ).to be_nil
   end
+
+  it 'rejects lowercase suspend/resume from core 3.1.4' do
+    message['aSp'] = "suspend"
+    expect( validate(message, 'core') ).to eq(
+      ["3.1.4", "3.1.5", "3.2.0", "3.2.1", "3.2.2"] => [["/aSp", "enum"]]
+    )
+  end
+
 end
