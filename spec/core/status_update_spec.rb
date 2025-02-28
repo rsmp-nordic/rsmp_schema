0f9e543abdbc10b19f822ce10e11d8cb71241a9d
Emil Tin <zf0f@tmf.kk.dk>
2025-02-28 13:14:30 +0100

    fix handling of s=null in status updates and response

--- spec/core/status_update_spec.rb
+++ spec/core/status_update_spec.rb
@@ -1,3 +1,6 @@
+# the 'undefined' value for q was added in 3.1.3
+# the 'undefined'
+
 RSpec.describe "Traffic Light Controller RSMP SXL Schema validation" do
   let(:message) {{
     "mType" => "rSMsg",
@@ -14,114 +17,128 @@ RSpec.describe "Traffic Light Controller RSMP SXL Schema validation" do
     expect(validate message, 'core').to be_nil
   end
 
-  it 'accepts valid status request with unkmown=q, s=null' do
-    message['sS'].first['q'] = 'unknown'
-    message['sS'].first['s'] = nil
-    expect(validate message, 'core').to be_nil
+  describe "q=recent" do 
+    it 'rejects s=null if q=recent' do
+      message["sS"].first["q"] = "recent"
+      message["sS"].first["s"] = nil
+      expect(validate message, 'core').to eq(
+        ["3.1.2", "3.1.3", "3.1.4", "3.1.5"] => [["/sS/0/s", "string"]],
+        ["3.2.0", "3.2.1", "3.2.2"] => [["/sS/0/s", "type"]],
+      )
+    end
   end
 
-  it 's must be null if q=unknown' do
-    message['sS'].first['q'] = 'unknown'
-    message['sS'].first['s'] = "1"
-    expect(validate message, 'core').to eq(
-      [["/sS/0/s", "null"]]
-    )
+  describe "q=undefined" do
+    it 'rejects s being null before 3.1.3' do
+      message["sS"].first["q"] = "undefined"   
+      message["sS"].first["s"] = nil
+      expect(validate message, 'core', '<3.1.3').to eq(
+        [["/sS/0/q", "enum"], ["/sS/0/s", "string"]])
+    end
+
+    it 'requires s being null from 3.1.3' do
+      message["sS"].first["q"] = "undefined"
+      message["sS"].first["s"] = "bad"
+      expect(validate message, 'core', '>=3.1.3').to eq(
+        [["/sS/0/s", "null"]]
+      )
+    end
+
+    it 'rejects s being string from 3.1.3' do
+      message["sS"].first["q"] = "undefined"
+      message["sS"].first["s"] = "something"
+      expect(validate message, 'core', '>=3.1.3').to eq( [["/sS/0/s", "null"]] )
+    end
   end
 
-  it 's cannot be null if q!=unknown' do
-    message['sS'].first['q'] = 'recent'
-    message['sS'].first['s'] = nil
-    expect(validate message, 'core').to eq(
-       ["3.1.2", "3.1.3", "3.1.4", "3.1.5"] => [["/sS/0/s", "string"]],
-       ["3.2.0", "3.2.1", "3.2.2"] => [["/sS/0/s", "type"]],
-    )
+  describe "q=unknown" do
+    it 'requires s being null from 3.1.3' do
+      message["sS"].first["q"] = "unknown"
+      message["sS"].first["s"] = nil
+      expect(validate message, 'core', '>=3.1.3').to be_nil
+    end
   end
 
-  it 'catches missing component id' do
-    message.delete 'cId'
-    expect(validate message, 'core').to eq(
-      [["", "required", {"missing_keys"=>["cId"]}]]
-    )
-  end
+  describe "argument check" do
+    it 'rejects missing component id' do
+      message.delete 'cId'
+      expect(validate message, 'core').to eq(
+        [["", "required", {"missing_keys"=>["cId"]}]]
+      )
+    end
 
-  it 'catches bad status code' do
-    message['sS'].first['sCI'] = '99'
-    expect(validate message, 'core').to eq(
-      [["/sS/0/sCI", "pattern"]]
-    )
-  end
+    it 'rejects bad status code' do
+      message['sS'].first['sCI'] = '99'
+      expect(validate message, 'core').to eq(
+        [["/sS/0/sCI", "pattern"]]
+      )
+    end
 
-  it 'catches missing sS' do
-    message.delete 'sS'
-    expect( validate(message, 'core') ).to eq(
-      [["", "required", {"missing_keys"=>['sS']}]]
-    )
-  end
+    it 'rejects empty sS array' do
+      message['sS'].clear
+      expect( validate(message, 'core') ).to eq(
+        [["/sS", "minItems"]]
+      )
+    end
 
-  it 'catches empty sS array' do
-    message['sS'].clear
-    expect( validate(message, 'core') ).to eq(
-      [["/sS", "minItems"]]
-    )
-  end
+    it 'rejects bad sS type' do
+      message['sS'] = {}
+      expect( validate(message, 'core') ).to eq(
+        [["/sS", "array"]]
+      )
+    end
 
-  it 'catches bad sS type' do
-    message['sS'] = {}
-    expect( validate(message, 'core') ).to eq(
-      [["/sS", "array"]]
-    )
-  end
+    it 'rejects missing status code' do
+      message['sS'].first.delete 'sCI'
+      expect(validate message, 'core').to eq(
+        [["/sS/0", "required", {"missing_keys"=>["sCI"]}]]
+      )
+    end
 
-  it 'catches missing status code' do
-    message['sS'].first.delete 'sCI'
-    expect(validate message, 'core').to eq(
-      [["/sS/0", "required", {"missing_keys"=>["sCI"]}]]
-    )
-  end
+    it 'rejects bad status code' do
+      message['sS'].first['sCI'] = 3
+      expect(validate message, 'core').to eq(
+        [["/sS/0/sCI", "string"]]
+      )
 
-  it 'catches bad status code' do
-    message['sS'].first['sCI'] = 3
-    expect(validate message, 'core').to eq(
-      [["/sS/0/sCI", "string"]]
-    )
+      message['sS'].first['sCI'] = '3'
+      expect(validate message, 'core').to eq(
+        [["/sS/0/sCI", "pattern"]]
+      )
+    end
 
-    message['sS'].first['sCI'] = '3'
-    expect(validate message, 'core').to eq(
-      [["/sS/0/sCI", "pattern"]]
-    )
-  end
+    it 'rejects missing name' do
+      message['sS'].first.delete 'n'
+      expect(validate message, 'core').to eq(
+        [["/sS/0", "required", {"missing_keys"=>["n"]}]]
+      )
+    end
 
-  it 'catches missing name' do
-    message['sS'].first.delete 'n'
-    expect(validate message, 'core').to eq(
-      [["/sS/0", "required", {"missing_keys"=>["n"]}]]
-    )
-  end
+    it 'rejects bad name' do
+      message['sS'].first['n'] = 3
+      expect(validate message, 'core').to eq(
+        [["/sS/0/n", "string"]]
+      )
+    end
 
-  it 'catches bad name' do
-    message['sS'].first['n'] = 3
-    expect(validate message, 'core').to eq(
-      [["/sS/0/n", "string"]]
-    )
-  end
-
-  it 'catches n set to null' do
-    message['sS'].first['n'] = nil
-    expect(validate message, 'core').to eq([["/sS/0/n", "string"]])
-  end
+    it 'rejects n being null' do
+      message['sS'].first['n'] = nil
+      expect(validate message, 'core').to eq([["/sS/0/n", "string"]])
+    end
 
 
-  it 'catches missing value' do
-    message['sS'].first.delete 's'
-    expect(validate message, 'core').to eq(
-      [["/sS/0", "required", {"missing_keys"=>["s"]}]]
-    )
-  end
+    it 'rejects missing value' do
+      message['sS'].first.delete 's'
+      expect(validate message, 'core').to eq(
+        [["/sS/0", "required", {"missing_keys"=>["s"]}]]
+      )
+    end
 
-  it 'catches bad quality' do
-    message['sS'].first['q'] = 'great'
-    expect(validate message, 'core').to eq(
-      [["/sS/0/q", "enum"]]
-    )
+    it 'rejects bad quality' do
+      message['sS'].first['q'] = 'great'
+      expect(validate message, 'core').to eq(
+        [["/sS/0/q", "enum"]]
+      )
+    end
   end
 end
